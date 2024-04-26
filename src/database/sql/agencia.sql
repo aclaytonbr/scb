16e26c2cf974f9bc556af6427bec2bb9605c13e2
aclaytonbr <rodrigues.anderson@gmail.com>
2024-04-26 11:16:13 -0400

    atualização das tabelas sql

--- src/database/sql/agencia.sql
+++ src/database/sql/agencia.sql
@@ -3,9 +3,9 @@
 CREATE TABLE `tbl_agencia` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `id_banco` int unsigned NOT NULL,
-  `numero` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `endereco` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `numero` varchar(10) CHARACTER SET NOT NULL,
+  `nome` varchar(100) CHARACTER SET NOT NULL,
+  `endereco` varchar(100) CHARACTER SET NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tbl_agencia_UN` (`numero`)
 )
--- src/database/sql/banco.sql
+++ src/database/sql/banco.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `tbl_banco` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Idenficador sequencial',
-  `numero` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `nome` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tbl_banco_UN` (`numero`)
 )
